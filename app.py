f08a50a51ca2a1ddfba4b282b430c2f7ead5e72d
hua0826 <67269657@qq.com>
2025-01-15 01:17:40 +0800

    add db connection pool and redis connection pool support

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from utils.redis_client import RedisClient
 async def db_session_middleware(request: Request, call_next):
     response = Response("Internal server error", status_code=500)
     try:
-        request.state.db = database_proxy.connection_context()
+        request.state.db = database_proxy
         response = await call_next(request)
     finally:
         request.state.db.close()
